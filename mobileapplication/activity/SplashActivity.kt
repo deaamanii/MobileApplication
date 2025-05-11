dfc698ea5b0cdfe435632b851b72a0335a51ada1
deaamanii <149594808+deaamanii@users.noreply.github.com>
2025-05-11 02:36:07 +0200

    Update
    Added Firebase to project, Splash Activity, Dashboard UI

---
+++ mobileapplication/activity/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.example.mobileapplication.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mobileapplication.MainActivity
+import com.example.mobileapplication.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 👇 This is where the click and toast logic goes
+        binding.startBtn.setOnClickListener {
+            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
